257383262a2ba006d2dac446abc0014d2f9c61a5
ElizavetaPaniushkina <s33591@pjwstk.edu.pl>
2025-05-16 15:09:59 +0200

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle();
-        Car car = new Car(5);
-        vehicle.start();
-        car.start();
-        car.stop();
-    }
-}
